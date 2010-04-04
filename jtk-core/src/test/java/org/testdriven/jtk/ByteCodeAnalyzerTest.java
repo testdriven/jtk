1f20f83dada4fca829761c0d76249bd081d54c6c
kcrimson <jpalka@gmail.com>
2010-04-04 21:09:55 +0200

    initial implementation of TestCaseByteAnalyzer, this is dummy implementation that only checks if @Test assertion is present before method, lots of TODOs :)

--- jtk-core/src/test/java/org/testdriven/jtk/ByteCodeAnalyzerTest.java
+++ jtk-core/src/test/java/org/testdriven/jtk/ByteCodeAnalyzerTest.java
@@ -1,44 +1,25 @@
 package org.testdriven.jtk;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 import java.io.FileInputStream;
 
 import org.junit.Test;
-import org.objectweb.asm.AnnotationVisitor;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.commons.EmptyVisitor;
 
 public class ByteCodeAnalyzerTest {
 
 	@Test
 	public void should_visit_test_methods() throws Exception {
-		ClassReader reader = new ClassReader(new FileInputStream(
-				"target/test-classes/org/testdriven/jtk/JTKTest.class"));
+		FileInputStream inputStream = new FileInputStream(
+				"target/test-classes/org/testdriven/jtk/JTKTest.class");
+		ByteCodeAnalyzer analyzer = new ByteCodeAnalyzer(inputStream);
 
-		reader.accept(new EmptyVisitor() {
+		String[] methods = analyzer.getTestMethods();
 
-			@Override
-			public MethodVisitor visitMethod(int arg0, String arg1,
-					String arg2, String arg3, String[] arg4) {
-				System.out.println(arg1);
-				return super.visitMethod(arg0, arg1, arg2, arg3, arg4);
-			}
-
-			@Override
-			public AnnotationVisitor visitAnnotation(String arg0, String arg1) {
-				// TODO Auto-generated method stub
-				System.out.println(arg0);
-				return super.visitAnnotation(arg0, arg1);
-			}
-
-			@Override
-			public AnnotationVisitor visitAnnotation(String arg0, boolean arg1) {
-				// TODO Auto-generated method stub
-				System.out.println(arg0);
-				return super.visitAnnotation(arg0, arg1);
-			}
-
-		}, 0);
+		assertThat(methods).containsOnly(
+				"should_throw_exception_if_base_dir_not_exists",
+				"should_create_test_case_analyzer", 
+				"should_find_test_cases");
 	}
 
 }
