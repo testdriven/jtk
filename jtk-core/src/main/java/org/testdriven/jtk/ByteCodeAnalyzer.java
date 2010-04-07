099a457ce44208435dcc001bc5628cf0316f6231
kcrimson <jpalka@gmail.com>
2010-04-07 12:22:41 -0500

    analyzer returns list of TestCaseMethod

--- jtk-core/src/main/java/org/testdriven/jtk/ByteCodeAnalyzer.java
+++ jtk-core/src/main/java/org/testdriven/jtk/ByteCodeAnalyzer.java
@@ -16,69 +16,83 @@ import org.objectweb.asm.commons.EmptyVisitor;
 
 public class ByteCodeAnalyzer {
 
-    private static final String JUNIT4_TEST_ANNOTATION = "org/junit/Test";
-    private final InputStream inputStream;
+	private static final String JUNIT4_TEST_ANNOTATION = "org/junit/Test";
+	private final InputStream inputStream;
 
-    public ByteCodeAnalyzer(InputStream inputStream) {
-        this.inputStream = inputStream;
+	public ByteCodeAnalyzer(InputStream inputStream) {
+		this.inputStream = inputStream;
 
-    }
+	}
 
-    public String[] getTestMethods() throws IOException {
-        ClassReader reader = new ClassReader(inputStream);
+	public TestCaseMethod[] getTestMethods() throws IOException {
+		ClassReader reader = new ClassReader(inputStream);
 
-        final Stack<String> methods = new Stack<String>();
-        final List<String> testMethodsNames = new ArrayList<String>();
+		final Stack<String> methods = new Stack<String>();
+		final List<TestCaseMethod> testMethodsNames = new ArrayList<TestCaseMethod>();
 
-        final MethodVisitor methodVisitor = new TestCaseMethodVisitor(methods, testMethodsNames);
-        final ClassVisitor classVisitor = new TestCaseClassVisitor(methods, methodVisitor);
+		final MethodVisitor methodVisitor = new TestCaseMethodVisitor(methods,
+				testMethodsNames);
+		final ClassVisitor classVisitor = new TestCaseClassVisitor(methods,
+				methodVisitor);
 
-        reader.accept(classVisitor, 0);
+		reader.accept(classVisitor, 0);
 
-        return testMethodsNames.toArray(new String[methods.size()]);
-    }
+		return testMethodsNames.toArray(new TestCaseMethod[methods.size()]);
+	}
 
-    private class TestCaseMethodVisitor extends EmptyVisitor {
+	private class TestCaseMethodVisitor extends EmptyVisitor {
 
-        private final Stack<String> methods;
-        private final List<String> testMethodsNames;
+		private final Stack<String> methods;
+		private final List<TestCaseMethod> testCaseMethods;
 
-        public TestCaseMethodVisitor(Stack<String> methods, List<String> testMethodsNames) {
-            this.methods = methods;
-            this.testMethodsNames = testMethodsNames;
-        }
+		public TestCaseMethodVisitor(Stack<String> methods,
+				List<TestCaseMethod> testCaseMethods) {
+			this.methods = methods;
+			this.testCaseMethods = testCaseMethods;
+		}
 
-        @Override
-        public AnnotationVisitor visitAnnotation(String name, boolean visible) {
-            System.out.println(format("visitAnnotation(%s,%b)", name, visible));
-            String methodName = methods.pop();
-            if (name.contains(JUNIT4_TEST_ANNOTATION)) {
-                testMethodsNames.add(methodName);
-            }
-            return super.visitAnnotation(name, visible);
-        }
+		@Override
+		public AnnotationVisitor visitAnnotation(String name, boolean visible) {
+			System.out.println(format("visitAnnotation(%s,%b)", name, visible));
+			String methodName = methods.pop();
+			if (name.contains(JUNIT4_TEST_ANNOTATION)) {
+				testCaseMethods.add(new TestCaseMethod(methodName));
+			}
+			return super.visitAnnotation(name, visible);
+		}
 
-        @Override
-        public AnnotationVisitor visitAnnotation(String arg0, String arg1) {
-            return super.visitAnnotation(arg0, arg1);
-        }
-    }
+		@Override
+		public AnnotationVisitor visitAnnotation(String arg0, String arg1) {
+			return super.visitAnnotation(arg0, arg1);
+		}
 
-    private static class TestCaseClassVisitor extends EmptyVisitor {
+		@Override
+		public void visitMethodInsn(int opcode, String owner, String name,
+				String desc) {
+			System.out.println(owner + " " + name + " "+desc);
+			super.visitMethodInsn(opcode, owner, name, desc);
+		}
 
-        private final Stack<String> methods;
-        private final MethodVisitor methodVisitor;
+	}
 
-        public TestCaseClassVisitor(Stack<String> methods, MethodVisitor methodVisitor) {
-            this.methods = methods;
-            this.methodVisitor = methodVisitor;
-        }
+	private static class TestCaseClassVisitor extends EmptyVisitor {
 
-        @Override
-        public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-            System.out.println(format("visitMethod(%d,%s,%s,%s,%s)", access, name, desc, signature, exceptions));
-            methods.push(name);
-            return methodVisitor;
-        }
-    }
+		private final Stack<String> methods;
+		private final MethodVisitor methodVisitor;
+
+		public TestCaseClassVisitor(Stack<String> methods,
+				MethodVisitor methodVisitor) {
+			this.methods = methods;
+			this.methodVisitor = methodVisitor;
+		}
+
+		@Override
+		public MethodVisitor visitMethod(int access, String name, String desc,
+				String signature, String[] exceptions) {
+			System.out.println(format("visitMethod(%d,%s,%s,%s,%s)", access,
+					name, desc, signature, exceptions));
+			methods.push(name);
+			return methodVisitor;
+		}
+	}
 }
--- jtk-core/src/test/java/org/testdriven/jtk/ByteCodeAnalyzerTest.java
+++ jtk-core/src/test/java/org/testdriven/jtk/ByteCodeAnalyzerTest.java
@@ -3,23 +3,36 @@ package org.testdriven.jtk;
 import static org.fest.assertions.Assertions.assertThat;
 
 import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
 import org.junit.Test;
 
 public class ByteCodeAnalyzerTest {
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void should_visit_test_methods() throws Exception {
 		FileInputStream inputStream = new FileInputStream(
 				"target/test-classes/org/testdriven/jtk/JTKTest.class");
 		ByteCodeAnalyzer analyzer = new ByteCodeAnalyzer(inputStream);
 
-		String[] methods = analyzer.getTestMethods();
+		TestCaseMethod[] testCaseMethods = analyzer.getTestMethods();
 
-		assertThat(methods).containsOnly(
+		Collection<String> testCaseNames = CollectionUtils.collect(Arrays
+				.asList(testCaseMethods), new Transformer() {
+
+			@Override
+			public Object transform(Object input) {
+				return ((TestCaseMethod) input).getMethodName();
+			}
+		});
+
+		assertThat(testCaseNames).containsOnly(
 				"should_throw_exception_if_base_dir_not_exists",
-				"should_create_test_case_analyzer", 
-				"should_find_test_cases");
+				"should_create_test_case_analyzer", "should_find_test_cases");
 	}
 
 }
