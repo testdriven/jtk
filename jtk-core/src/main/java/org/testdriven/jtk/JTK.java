1403b0318f8207ea5ef8d19c0d42b9fc0d5ccb9f
jpalka@gmail.com <jpalka@gmail.com>
2010-05-16 13:26:42 +0200

    - refactored JTK entry class

--- jtk-core/src/main/java/org/testdriven/jtk/JTK.java
+++ jtk-core/src/main/java/org/testdriven/jtk/JTK.java
@@ -1,73 +1,32 @@
 package org.testdriven.jtk;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.testdriven.jtk.junit4.JUnit4AnalisysEngine;
 
 public class JTK {
 
-	private static final String TEST_JAVA_SUFFIX = "Test.java";
+    private static final String TEST_JAVA_SUFFIX = "Test.java";
+    private final File sourcesDir;
+    private final File classesDir;
 
-	private static final FileFilter TEST_CASES_FILTER = new FileFilter() {
+    public JTK(String sourcedir, String classesdir) throws IOException {
+        this.sourcesDir = new File(sourcedir);
 
-		public boolean accept(File pathname) {
-			return pathname.isFile()
-					&& pathname.getAbsolutePath().endsWith(TEST_JAVA_SUFFIX);
-		}
-	};
+        if (!sourcesDir.exists() || !sourcesDir.isDirectory()) {
+            throw new IOException("Sources directory " + sourcesDir.getAbsolutePath() + "doesn't exists");
+        }
 
-	private static final FileFilter DIRECTORY_FILTER = new FileFilter() {
+        this.classesDir = new File(classesdir);
+        if (!classesDir.exists() || !classesDir.isDirectory()) {
+            throw new IOException("Classes directory " + classesDir.getAbsolutePath() + "doesn't exists");
+        }
+    }
 
-		public boolean accept(File pathname) {
-			return pathname.isDirectory();
-		}
-	};
-
-	private File baseDir;
-
-	public JTK(String dir) throws IOException {
-		this.baseDir = new File(dir);
-		
-		if(!baseDir.exists()||!baseDir.isDirectory()){
-			throw new IOException("Directory " + baseDir.getAbsolutePath()+"doesn't exists");
-		}
-	}
-
-	public File[] findTestCases() {
-		return findTestCases(baseDir);
-
-	}
-
-	private File[] findTestCases(File directory) {
-		// simple implementation that checks filename pattern
-		// TODO in future it should be delegated to engine that will recognize
-		// JUnit3, JUnit4 and TestNG tests
-		List<File> testCases = new ArrayList<File>();
-
-		File[] files = directory.listFiles(TEST_CASES_FILTER);
-		testCases.addAll(Arrays.asList(files));
-
-		// dive deep into directory structure
-
-		File[] directories = directory.listFiles(DIRECTORY_FILTER);
-
-		for (File dir : directories) {
-			files = findTestCases(dir);
-			testCases.addAll(Arrays.asList(files));
-		}
-
-		return testCases.toArray(new File[testCases.size()]);
-	}
-
-	public TestCaseAnalyzer createTestCaseAnalyzer() {
-		return createTestCaseAnalyzer(findTestCases());
-	}
-
-	public TestCaseAnalyzer createTestCaseAnalyzer(File[] testCases) {
-		return new TestCaseAnalyzer(null,null,null);
-	}
+    public TestCaseAnalyzer createTestCaseAnalyzer() {
+        AnalisysEngine engine = new JUnit4AnalisysEngine(null);
+        String[] sourcesDirs = new String[]{sourcesDir.getAbsolutePath()};
+        String[] classesDirs = new String[]{classesDir.getAbsolutePath()};
+        return new TestCaseAnalyzer(null, sourcesDirs, classesDirs);
+    }
 }
--- jtk-core/src/test/java/org/testdriven/jtk/JTKTest.java
+++ jtk-core/src/test/java/org/testdriven/jtk/JTKTest.java
@@ -2,41 +2,35 @@ package org.testdriven.jtk;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.fest.assertions.Assertions;
 import org.junit.Test;
 
 public class JTKTest {
 
-	@Test
-	public void should_find_test_cases() throws Exception {
-		// given
-		String sourcedir = "src/test/java";
-		String classdir = "";
-		JTK jtk = new JTK(sourcedir);
-		// when
-		File[] testCases = jtk.findTestCases();
-		// then
-		Assertions.assertThat(testCases).hasSize(4);
-	}
+    @Test(expected = IOException.class)
+    public void should_throw_exception_if_source_dir_not_exists()
+            throws Exception {
+        String sourcedir = "test/java";
+        String classesdir = "target/test-classes";
+        JTK jtk = new JTK(sourcedir, classesdir);
+    }
 
-	@Test(expected = IOException.class)
-	public void should_throw_exception_if_base_dir_not_exists()
-			throws Exception {
-		String dir = "test/java";
-		JTK jtk = new JTK(dir);
-	}
+    @Test(expected = IOException.class)
+    public void should_throw_exception_if_classes_dir_not_exists() throws Exception {
+        String sourcedir = "src/test/java";
+        String classesdir = "test-classes";
+        JTK jtk = new JTK(sourcedir, classesdir);
+    }
 
-	@Test
-	public void should_create_test_case_analyzer() throws Exception {
-		// given
-		String dir = "src/test/java";
-		JTK jtk = new JTK(dir);
-		TestCaseAnalyzer analyzer = jtk.createTestCaseAnalyzer();
+    @Test
+    public void should_create_test_case_analyzer() throws Exception {
+        // given
+        String sourcedir = "src/test/java";
+        String classesdir = "target/test-classes";
+        JTK jtk = new JTK(sourcedir, classesdir);
+        TestCaseAnalyzer analyzer = jtk.createTestCaseAnalyzer();
 
-		assertThat(analyzer).isNotNull();
-	}
-
+        assertThat(analyzer).isNotNull();
+    }
 }
