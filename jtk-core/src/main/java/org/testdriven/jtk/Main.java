3e42d32861a8eedf8f973cc6fa5a2543e1eb5799
jpalka@gmail.com <jpalka@gmail.com>
2010-05-16 21:45:50 +0200

    - inital Main implementation

--- jtk-core/src/main/java/org/testdriven/jtk/Main.java
+++ jtk-core/src/main/java/org/testdriven/jtk/Main.java
@@ -5,6 +5,15 @@ public final class Main {
 	/**
 	 * Main application entry point
 	 */
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws  Exception{
+            JTKCommandParser parser = new JTKCommandParser(args);
+            JTKCommandOptions options = parser.parse();
+
+            JTK jtk = new JTK(options.getSources()[0],options.getClasses()[0]);
+            TestCaseAnalyzer analyzer = jtk.createTestCaseAnalyzer();
+            AnalyzerResults results = analyzer.analyzeTestCases();
+
+            AnalyzerResultsPrinter printer = new AnalyzerResultsPrinter(System.out);
+            printer.writeSummary(results);
 	}
 }
